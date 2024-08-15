08da63d7e4805408686250c636cf450981c0e280
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-08-15 18:23:02 +0400

    62

--- themoviedb/lib/main.dart
+++ themoviedb/lib/main.dart
@@ -1,63 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:themoviedb/ui/Theme/app_colors.dart';
-import 'package:themoviedb/ui/widgets/auth/auth_widget.dart';
-import 'package:themoviedb/ui/widgets/main_screen/main_screen_widget.dart';
-import 'package:themoviedb/ui/widgets/movie_details/movie_details_widget.dart';
+import 'package:themoviedb/ui/widgets/app/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: AppColors.mainDarkBlue),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: AppColors.mainDarkBlue,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
-        ),
-      ),
-      routes: {
-        '/auth': (context) => const AuthWidget(),
-        '/main_screen': (context) => const MainScreenWidget(),
-        '/main_screen/movie_details': (context) {
-          final arguments = ModalRoute.of(context)?.settings.arguments;
-          if (arguments is int) {
-            return MovieDetailsWidget(Movieid: arguments);
-          } else {
-            return MovieDetailsWidget(Movieid: 0);
-          }
-
-          // final id = ModalRoute.of(context)?.settings.arguments as int;
-          // return MovieDetailsWidget(Movieid: id);
-        }
-
-      },
-      initialRoute: '/auth',
-      //routingi tekrarlamaq ucun
-
-      // onGenerateRoute: (RouteSettings settings) {
-      //   return MaterialPageRoute<void>(
-      //     builder: (context) {
-      //       return Scaffold(
-      //         appBar: AppBar(
-      //           title: const Text('Error'),
-      //         ),
-      //         body: Center(
-      //           child: Text('No route defined for ${settings.name}'),
-      //         ),
-      //       );
-      //     },
-      //   );
-      // },
-    );
-  }
 }
---
+++ themoviedb/lib/ui/widgets/app/my_app.dart
@@ -0,0 +1,58 @@
+import 'package:themoviedb/ui/Theme/app_colors.dart';
+import 'package:themoviedb/ui/widgets/auth/auth_widget.dart';
+import 'package:themoviedb/ui/widgets/main_screen/main_screen_widget.dart';
+import 'package:themoviedb/ui/widgets/movie_details/movie_details_widget.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(backgroundColor: AppColors.mainDarkBlue),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
+      routes: {
+        '/auth': (context) => const AuthWidget(),
+        '/main_screen': (context) => const MainScreenWidget(),
+        '/main_screen/movie_details': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments;
+          if (arguments is int) {
+            return MovieDetailsWidget(Movieid: arguments);
+          } else {
+            return MovieDetailsWidget(Movieid: 0);
+          }
+
+          // final id = ModalRoute.of(context)?.settings.arguments as int;
+          // return MovieDetailsWidget(Movieid: id);
+        }
+      },
+      initialRoute: '/auth',
+      //routingi tekrarlamaq ucun
+
+      // onGenerateRoute: (RouteSettings settings) {
+      //   return MaterialPageRoute<void>(
+      //     builder: (context) {
+      //       return Scaffold(
+      //         appBar: AppBar(
+      //           title: const Text('Error'),
+      //         ),
+      //         body: Center(
+      //           child: Text('No route defined for ${settings.name}'),
+      //         ),
+      //       );
+      //     },
+      //   );
+      // },
+    );
+  }
+}
