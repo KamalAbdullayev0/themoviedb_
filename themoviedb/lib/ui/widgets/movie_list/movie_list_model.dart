39d50de49707ac915e0ce136c28ebdf92672dbae
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-09-02 18:15:11 +0400

    axtaris building

--- themoviedb/lib/ui/widgets/movie_list/movie_list_model.dart
+++ themoviedb/lib/ui/widgets/movie_list/movie_list_model.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:themoviedb/domain/api_client/api_client.dart';
 import 'package:themoviedb/domain/entity/movie.dart';
+import 'package:themoviedb/domain/entity/popular_movie_response.dart';
 import 'package:themoviedb/ui/navigation/main_navigation.dart';
 
 class MovieListModel extends ChangeNotifier {
@@ -13,27 +16,42 @@ class MovieListModel extends ChangeNotifier {
   List<Movie> get movies => List.unmodifiable(_movies);
   late DateFormat _dateFormat;
   String _locale = '';
+  String? _searchQuery;
+  Timer? searchDebounce;
 
   String stringFromDate(DateTime? date) =>
       date == null ? '' : _dateFormat.format(date);
 
-  void setupLocale(BuildContext context) {
+  void setupLocale(BuildContext context) async {
     final locale = Localizations.localeOf(context).toLanguageTag();
     if (locale == _locale) return;
     _locale = locale;
     _dateFormat = DateFormat.yMMMd(locale);
+    await _resetList();
+  }
+
+  Future<void> _resetList() async {
     _totalPages = 1;
     _currentPage = 0;
     _movies.clear();
-    _loadMovies();
+    await _loadNextPage();
   }
 
-  Future<void> _loadMovies() async {
+  Future<PopularMovieResponse> _loadMovies(int nextPage, String locale) async {
+    final query = _searchQuery;
+    if (query == null) {
+      return await _apiClient.popularMovie(nextPage, locale);
+    } else {
+      return await _apiClient.searchMovie(nextPage, locale, query);
+    }
+  }
+
+  Future<void> _loadNextPage() async {
     if (_isLoadingInProgress || _currentPage >= _totalPages) return;
     _isLoadingInProgress = true;
     final nextPage = _currentPage + 1;
     try {
-      final moviesResponse = await _apiClient.popularMovie(nextPage, _locale);
+      final moviesResponse = await _loadMovies(nextPage, _locale);
       _movies.addAll(moviesResponse.movies);
       _currentPage = moviesResponse.page;
       _totalPages = moviesResponse.totalPages;
@@ -51,8 +69,18 @@ class MovieListModel extends ChangeNotifier {
         arguments: id);
   }
 
+  Future<void> searchMovie(String text) async {
+    searchDebounce?.cancel();
+    searchDebounce = Timer(const Duration(milliseconds: 500), () async {
+      final searchQuery = text.isNotEmpty ? text : null;
+      if (_searchQuery == searchQuery) return;
+      _searchQuery = searchQuery;
+      await _resetList();
+    });
+  }
+
   void showedMovieAtIndex(int index) {
     if (index == _movies.length - 1) return;
-    _loadMovies();
+    _loadNextPage();
   }
 }
